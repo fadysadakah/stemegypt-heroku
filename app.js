8e0bc7559cd3e9b7f2ddb19bf57635d45fd62dce
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-04-07 13:46:39 +0200

    status code

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var express = require("express"), app = express(), request = require("request"),
 app.set("view engine", "ejs"), app.get("/", function (req, res) {
     
     res.render("index")
-}), app.get("*", function (req, res) { res.render("error"); res.status(404); }), app.listen(PORT, function () { console.log("Server Started") }); 
+}), app.get("*", function (req, res) { res.status(404); res.render("error"); }), app.listen(PORT, function () { console.log("Server Started") }); 
