1ac25b77dccf62bc552858b3245d28a58ae17e2e
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-04-07 13:44:24 +0200

    change status code

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var express = require("express"), app = express(), request = require("request"),
  app.use(bodyParser.urlencoded({ extended: !1 })), app.use(express.static("public")); 
  const PORT = process.env.PORT || 5e3; 
 app.set("view engine", "ejs"), app.get("/", function (req, res) {
-    res.status(404);
- res.render("index") }), app.get("*", function (req, res) { res.render("error") }), app.listen(PORT, function () { console.log("Server Started") }); 
+    
+    res.render("index")
+}), app.get("*", function (req, res) { res.render("error"); res.status(404); }), app.listen(PORT, function () { console.log("Server Started") }); 
