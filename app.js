ad305c3c3cdda5f2d9a338efe22d19df5a5479eb
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-04-08 20:07:14 +0200

    sign up

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-var express = require("express"), app = express(), request = require("request"), bodyParser = require("body-parser"); 
- app.use(bodyParser.urlencoded({ extended: !1 })), app.use(express.static("public")); 
- const PORT = process.env.PORT || 5e3; 
-app.set("view engine", "ejs"), app.get("/", function (req, res) {
-    
+var express = require("express");
+var app = express();
+var request = require("request");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: !1 }));
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 5001;
+
+
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function (req, res) {
+
     res.render("index")
-}), app.get("*", function (req, res) { res.status(404); res.render("error"); }), app.listen(PORT, function () { console.log("Server Started") }); 
+});
+
+
+
+app.get("/signup", function (req, res) {
+
+    res.render("sign_up")
+});
+
+
+app.post("/signup", function (req, res) {
+
+    res.render("sign_up")
+});
+
+
+app.get("*", function (req, res) {
+    res.status(404);
+    res.render("error");
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("Server Started")
+});
